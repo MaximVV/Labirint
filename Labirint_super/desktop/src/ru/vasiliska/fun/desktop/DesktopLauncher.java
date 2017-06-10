70dfe6ad25b7b37e25f200592b2ebe481b2b3d6a
MaximVV <laul@bk.ru>
2017-06-10 15:04:59 +0400

    first

---
+++ Labirint_super/desktop/src/ru/vasiliska/fun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.vasiliska.fun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.vasiliska.fun.Labirint;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Labirint(), config);
+	}
+}
